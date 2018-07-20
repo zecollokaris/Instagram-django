122d30941770a6969bcf00c27fe2ad28f4b4bd7e
zecollokaris <collo.kariss@gmail.com>
2018-07-20 16:39:26 +0300

    created image models

--- gram/models.py
+++ gram/models.py
@@ -47,3 +47,35 @@ class Profile(models.Model):
         return self.user.user
 
 
+
+#################################################################################################################################################################################################################################
+
+#...Class IMAGE added here...
+
+
+class Image(models.Model):
+#Attribute Variables for Image class to represent different columns in database 
+    '''
+    -image_uploader-: (this is the profile of person, uploading the image which is ,-linked to profile using 
+    foreign key,-which is then linked to imported user at the top (for ease in querying the database the image creator
+    is used which is linked directly to user)
+
+    -image_likes-: This is attribute created with a (ManyToManyField)-to allow many users to like one picture
+
+    -image_comments-: This is attribute created with a (ManyToManyField)-to allow many users to comment on one picture and still get their profiles
+
+    -date-: imported as a django relative import ('from django.contrib.auth.models import User'))
+        set to auto using python model, this allows for date to be added automaticaly
+
+    -imageuploader_profile-: this is the person who posted the picture (User)
+        '''
+    image = models.ImageField(upload_to ='picsagram/')
+    image_caption = models.CharField(max_length=700)
+    tag_someone = models.CharField(max_length=50,blank=True)
+    imageuploader_profile = models.ForeignKey('Profile', on_delete=models.CASCADE,null='True', blank=True)
+    image_likes = models.ManyToManyField('Profile', default=False, blank=True, related_name='likes')
+    date = models.DateTimeField(auto_now_add=True, null= True)
+
+    '''Method to filter database results'''
+    def __str__(self):
+        return self.image_caption
