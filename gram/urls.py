98e475c2496093f04143c529bd0cbde3f28487fd
zecollokaris <collo.kariss@gmail.com>
2018-07-20 15:41:18 +0300

    Introduced index url

---
+++ gram/urls.py
@@ -0,0 +1,38 @@
+from django.urls import path
+''' 
+This is to allow us to connect the PROJECT's  url with this APP's url 
+it will then allow us to define paths here as we had included (grams urls)
+in the PROJECT.
+'''
+
+from . import views
+'''
+Here we are importing the views as we shall connect templates from there 
+'''
+
+from django.conf import settings
+'''
+This is importing the settings configurations to (gramapp)
+'''
+
+from django.conf.urls.static import static
+'''
+This is to import static-files in urls
+'''
+
+
+'''End Of Import'''
+#---------------------------------------------------------------------#
+
+urlpatterns=[
+
+
+
+#################################################################################################################################################################################
+#URL FOR  HOME PAGE
+#################################################################################################################################################################################
+
+    #HOME Page url!
+    #This is the home page url pattern 
+    path('',views.index, name='index'),
+]
--- insta/urls.py
+++ insta/urls.py
@@ -3,6 +3,11 @@
 from django.contrib import admin
 
 from django.urls import path, include
+
+from gram import views
+
+from django.conf.urls.static import static
+
 '''Here we have imported (include)-: this allows us to add the gram url pattern
 as done below this is to avoid filling up all our urls in this page'''
 
