73a9a04989b6eb21f41d38ad017ad612b37cdf81
zecollokaris <collo.kariss@gmail.com>
2018-07-24 14:08:11 +0300

    added setting to upload image creating views and url

--- gram/models.py
+++ gram/models.py
@@ -66,7 +66,7 @@ class Image(models.Model):
 
     -imageuploader_profile-: this is the person who posted the picture (User)
         '''
-    image = models.ImageField(upload_to ='picsagram/')
+    image = models.ImageField(upload_to ='pictsagram/')
     image_caption = models.CharField(max_length=700)
     tag_someone = models.CharField(max_length=50,blank=True)
     imageuploader_profile = models.ForeignKey('Profile', on_delete=models.CASCADE,null='True', blank=True)
--- insta/settings.py
+++ insta/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -131,3 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 #Login Redirecting to Home
 LOGIN_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
