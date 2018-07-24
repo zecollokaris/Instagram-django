2cfc429a6308debbf0079dda6c0af9e3c709d91c
zecollokaris <collo.kariss@gmail.com>
2018-07-24 14:51:38 +0300

    added class forms for created form.py

---
+++ gram/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Profile, Image
+from django.contrib.auth.models import User
+
+class PostForm(forms.Modelform):
+    class Meta:
+        model = Image
+        fields = ('image_caption', 'image', 'tag_someone',)
+
+class SignUpForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    exclude = ['bio','profile_pic','profile_avatar','date']
