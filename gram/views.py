2a8af0d223ada54dcd4b463b067efaf9921f3ff7
zecollokaris <collo.kariss@gmail.com>
2018-07-25 22:45:58 +0300

    working on like section

--- gram/views.py
+++ gram/views.py
@@ -34,9 +34,10 @@ authenticated users
 @login_required(login_url='/accounts/login/')
 def index(request):
     all_images = Image.objects.all()
+    all_users = Profile.objects.all()
     next = request.GET.get('next')
     if next: return redirect(next)
-    return render(request, 'display/home.html',  {"all_images": all_images})
+    return render(request, 'display/home.html',  {"all_images": all_images}, {"all_users":all_users})
 
 #################################################################################################################################################################################
 #EXPLORE PAGE VIEW FUNCTION
