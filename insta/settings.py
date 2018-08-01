9b4cc0dcd47330df7bfc1a186ad6865b26ed870b
zecollokaris <collo.kariss@gmail.com>
2018-08-01 12:45:29 +0300

    Deployment attempt 3

--- insta/settings.py
+++ insta/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool) 
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
